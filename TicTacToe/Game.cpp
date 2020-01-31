052277b20d1934547543bd69e0a27a0ae3bb8785
Patrick Gilfoil <s198018@students.aie.edu>
2020-01-31 10:48:52 -0600

    blugh

--- TicTacToe/Game.cpp
+++ TicTacToe/Game.cpp
@@ -25,8 +25,9 @@ void Game::draw() {
 
 void Game::getInput() {
 	char input = '0';
-		
-	while (player1Turn) {
+	while (!gameOver) {
+		//do a turn, then check to see if anyone won
+		while (player1Turn) {
 			draw();
 			std::cout << "Player 1, choose where to mark: " << YELLOW;
 
@@ -64,12 +65,17 @@ void Game::getInput() {
 				grid[2][2] = 'X';
 			}
 
-			//checkForWin();
-			player1Turn = false;
-			player2Turn = true;
+			if (gameOver = checkForWin()) {
+				player1Turn = false;
+				player2Turn = false;
+			}
+			else {
+				player2Turn = true;
+				player1Turn = false;
+			}
 		}
-
-	while (player2Turn) {
+		//same thing for player 2's turn
+		while (player2Turn) {
 			draw();
 			std::cout << "Player 2, choose where to mark: " << YELLOW;
 
@@ -107,14 +113,87 @@ void Game::getInput() {
 				grid[2][2] = 'O';
 			}
 
-			//checkForWin();
-			player2Turn = false;
-			player1Turn = true;
+			if (gameOver = checkForWin()) {
+				player1Turn = false;
+				player2Turn = false;
+			}
+			else {
+				player1Turn = true;
+				player2Turn = false;
+			}
 		}
-	
+	}
 }
 
 bool Game::checkForWin() {
 	//player 1
-	return false;
+	if (grid[0][0] == 'X' && grid[0][1] == 'X' && grid[0][2] == 'X') {
+		std::cout << "Player 1 wins." << std::endl;
+		return true;
+	}
+	if (grid[1][0] == 'X' && grid[1][1] == 'X' && grid[1][2] == 'X') {
+		std::cout << "Player 1 wins." << std::endl;
+		return true;
+	}
+	if (grid[2][0] == 'X' && grid[2][1] == 'X' && grid[2][2] == 'X') {
+		std::cout << "Player 1 wins." << std::endl;
+		return true;
+	}
+	if (grid[0][0] == 'X' && grid[1][0] == 'X' && grid[2][0] == 'X') {
+		std::cout << "Player 1 wins." << std::endl;
+		return true;
+	}
+	if (grid[0][1] == 'X' && grid[1][1] == 'X' && grid[2][1] == 'X') {
+		std::cout << "Player 1 wins." << std::endl;
+		return true;
+	}
+	if (grid[0][2] == 'X' && grid[1][2] == 'X' && grid[2][2] == 'X') {
+		std::cout << "Player 1 wins." << std::endl;
+		return true;
+	}
+	if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid[2][2] == 'X') {
+		std::cout << "Player 1 wins." << std::endl;
+		return true;
+	}
+	if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid[2][0] == 'X') {
+		std::cout << "Player 1 wins." << std::endl;
+		return true;
+	}
+	//player 2
+	if (grid[0][0] == 'O' && grid[0][1] == 'O' && grid[0][2] == 'O') {
+		std::cout << "Player 2 wins.";
+		return true;
+	}
+	if (grid[1][0] == 'O' && grid[1][1] == 'O' && grid[1][2] == 'O') {
+		std::cout << "Player 2 wins.";
+		return true;
+	}
+	if (grid[2][0] == 'O' && grid[2][1] == 'O' && grid[2][2] == 'O') {
+		std::cout << "Player 2 wins.";
+		return true;
+	}
+	if (grid[0][0] == 'O' && grid[1][0] == 'O' && grid[2][0] == 'O') {
+		std::cout << "Player 2 wins.";
+		return true;
+	}
+	if (grid[0][1] == 'O' && grid[1][1] == 'O' && grid[2][1] == 'O') {
+		std::cout << "Player 2 wins.";
+		return true;
+	}
+	if (grid[0][2] == 'O' && grid[1][2] == 'O' && grid[2][2] == 'O') {
+		std::cout << "Player 2 wins.";
+		return true;
+	}
+	if (grid[0][0] == 'O' && grid[1][1] == 'O' && grid[2][2] == 'O') {
+		std::cout << "Player 2 wins.";
+		return true;
+	}
+	if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid[2][0] == 'O') {
+		std::cout << "Player 2 wins.";
+		return true;
+	}
+	else {
+		return false;
+	}
+	
 }
